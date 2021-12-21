343a974aef1be2cedac9aef0222792212e3a88ab
Lorenzo Gaifas <brisvag@gmail.com>
2021-12-21 11:08:35 +0100

    initial commit

---
+++ src/napari_molecule_reader/__init__.py
@@ -0,0 +1,10 @@
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+from ._reader import napari_get_reader
+
+from ._sample_data import make_sample_data 
---
+++ src/napari_molecule_reader/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from napari_molecule_reader import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
